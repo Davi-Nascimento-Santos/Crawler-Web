ec321d33850495a21019cf547a38990170f9572d
Davi Nascimento <davinascimento19072001@gmail.com>
2026-01-03 11:30:34 -0300

    create function crawlPage

---
+++ source/main.js
@@ -0,0 +1,17 @@
+process.argv
+const {crawlPage} = require('./normalizeURL');
+async function main(){
+    if (process.argv.length < 3){
+        console.log("Error, don't pass the url parameter");
+    }
+    if (process.argv.length > 3){
+        console.log("Error, pass too many arguments");
+    }
+    try{
+        const result = await crawlPage(process.argv[2], {});
+        console.log(result);
+    }catch(err){
+        console.log(err);
+    }
+}
+main()
--- source/normalizeURL.js
+++ source/normalizeURL.js
@@ -1,24 +1,44 @@
 const {JSDOM} = require('jsdom');
 
+async function crawlPage(baseURL, pages){
+    try{
+        const data = await fetch(baseURL);
+        console.log(`Starting crawling on ${baseURL}`)
+        if (data.headers.get('content-type').includes('text/html')){
+            const html = await data.text();
+            const urls = getURLsFromHTML(html, baseURL);
+            for(const url of urls){
+                pages[url] = 1;
+            }
+        }else{
+            throw new Error("Error, no HTML page");
+        }
+    }catch(err){
+        console.log(`Error, message: ${err.message}`)
+    }
+    return pages;
+
+}
+
 function getURLsFromHTML(html, baseURL){
     const dom = new JSDOM(html);
     const as = dom.window.document.querySelectorAll('a');
     const urls = [];
     for (const a of as){
-        console.log(a.href);
         if (a.href.slice(0, 1) === '/'){
             try{
-                const fullURLObj = new URL(`${baseURL}${a.href}`);
+                const newURL = `${baseURL}${a.href}`;
+                const fullURLObj = new URL(newURL);
                 urls.push(`${fullURLObj.href}`)
             }catch(err){
-                console.log("error, relative path invalid")
+                console.log(`error with message: ${err}, relative path invalid on ${a.href}`)
             }
         }else{
             try{
                 const fullURLObj = new URL(a.href);
                 urls.push(fullURLObj.href);
             }catch(err){
-                console.log("Error, absolute path invalid")
+                console.log(`Error with message: ${err}, absolute path invalid on ${a.href}`)
             }
             
         }
@@ -34,4 +54,4 @@ function normalizeURL(url){
     }
     return fullURL
 }
-module.exports = {normalizeURL, getURLsFromHTML};
+module.exports = {normalizeURL, getURLsFromHTML, crawlPage};
